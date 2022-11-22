83a46b242d12dc0e0f1cb204b4a5afa75b44f7f4
Prabhjeet Singh <prabhjeet93cs@gmail.com>
2022-11-21 20:07:04 -0500

    no message

---
+++ test.py
@@ -0,0 +1,15 @@
+print('Test Automation Framework')
+
+""" Hit Below API for the automation
+
+Get request
+https://api.apis.guru/v2/specs/abstractapi.com/geolocation/1.0.0/openapi.json
+http://www.unamourdeyorkshire.com/wp-json/wp/v2/users/2/index.json
+https://www.reddit.com/dev/api
+
+Try these urls -
+https://rapidapi.com/tuttotone/api/t-one-youtube-converter/
+https://rapidapi.com/search/
+https://apis.guru/
+
+"""
